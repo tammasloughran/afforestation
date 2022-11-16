0160a31caec21df4734051948c1d05bcaf1b1b8d
tammasloughran <tammas.loughran@csiro.au>
2022-11-16 17:06:21 +1100

    Added script to plot the atmospheric co2 concentration for all models

---
+++ analysis/plot_models_co2.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python3
+"""Plot the atmosphere co2 concentrations for all models.
+"""
+import os
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+import netCDF4 as nc
+from cdo import Cdo
+import cdo_decorators as cdod
+if __name__ == 'analysis.plot_afforestation':
+    # plot_afforestation.py imported as a module of the analysis package.
+    from analysis.cmip_files import get_filenames
+    from analysis.constants import (
+            C_IN_CO2_RATIO,
+            DATA_DIR,
+            DPI,
+            ENSEMBLES,
+            KG_IN_PG,
+            MASS_ATM, # kg
+            PLOTS_DIR,
+            MIL,
+            MOLMASS_CO2, # kg/mol
+            MOLMASS_O2, # kg/mol
+            MOLMASS_AIR, # kg/mol
+            KGKG_TO_MOLMOL, # Converion of kg/kg to mol/mol
+            NENS,
+            NTIMES,
+            SEC_IN_DAY,
+            )
+else:
+    # plot_afforestation.py is main program or imported as a module from another script.
+    from cmip_files import get_filenames
+    from constants import (
+            C_IN_CO2_RATIO,
+            DATA_DIR,
+            DPI,
+            ENSEMBLES,
+            KG_IN_PG,
+            MASS_ATM, # kg
+            PLOTS_DIR,
+            MIL,
+            MOLMASS_CO2, # kg/mol
+            MOLMASS_O2, # kg/mol
+            MOLMASS_AIR, # kg/mol
+            KGKG_TO_MOLMOL, # Converion of kg/kg to mol/mol
+            NENS,
+            NTIMES,
+            SEC_IN_DAY,
+            )
+import ipdb
+
+cdo = Cdo()
+cdo.debug = False
+color_cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+# Constants
+CO2_VARIABLES = {
+        'Emon':'co23D', # CO2 concentration (mixing ratio) in kg/kg.
+        'Amon':'co2', # Mole fraction of CO2. esm-ssp585 only
+        }
+MODELS = [ # Some models have missing co2 data for one of the experiments.
+        #'ACCESS-ESM1-5',
+        'BCC-CSM2-MR',
+        #'CanESM5',
+        #'GFDL-ESM4',
+        'MIROC-ES2L',
+        'MPI-ESM1-2-LR',
+        'NorESM2-LM',
+        #'UKESM1-0-LL',
+        ]
+INSTIT = {
+        'ACCESS-ESM1-5':'CSIRO',
+        'BCC-CSM2-MR':'BCC',
+        'CanESM5':'CCma',
+        'GFDL-ESM4':'NOAA-GFDL',
+        'MIROC-ES2L':'MIROC',
+        'MPI-ESM1-2-LR':'MPI-M',
+        'NorESM2-LM':'NCC',
+        'UKESM1-0-LL':'MOHC',
+        }
+ENSEMBLES = {
+        'ACCESS-ESM1-5':'r1i1p1f1',
+        'BCC-CSM2-MR':'r1i1p1f1',
+        'CanESM5':'r1i1p2f1',
+        'MIROC-ES2L':'r1i1p1f2',
+        'UKESM1-0-LL':'r1i1p1f2',
+        'MPI-ESM1-2-LR':'r1i1p1f1',
+        'NorESM2-LM':'r1i1p1f1',
+        'GFDL-ESM4':'r1i1p1f1',
+        }
+A_OR_AER = {
+        'ACCESS-ESM1-5':'Amon',
+        'BCC-CSM2-MR':'Amon',
+        'CanESM5':'Amon',
+        'GFDL-ESM4':'Amon',
+        'MIROC-ES2L':'AERmon',
+        'MPI-ESM1-2-LR':'Amon',
+        'NorESM2-LM':'AERmon',
+        'UKESM1-0-LL':'Amon',
+        }
+COLORS = {
+        'CSIRO':color_cycle[0],
+        'BCC':color_cycle[1],
+        'CCma':color_cycle[2],
+        'NOAA-GFDL':color_cycle[3],
+        'MIROC':color_cycle[4],
+        'MPI-M':color_cycle[5],
+        'NCC':color_cycle[6],
+        'MOHC':color_cycle[8],
+        }
+
+load_cdo = False
+load_npy = not load_cdo
+
+
+@cdod.cdo_cat(input2='')
+@cdod.cdo_vertmean
+@cdod.cdo_fldmean(weights='TRUE')
+@cdod.cdo_yearmonmean
+def global_average(var:str, input:str)->np.ma.MaskedArray:
+    """Load files field and vertical aggregated yearly data using CDO.
+    """
+    return cdo.copy(input=input, options='-L', returnCdf=True).variables[var][:].squeeze()
+
+
+def make_co2_models_plot()->None:
+    """Make the model intercomparison plot of co2 concentration.
+    """
+    global for_co2_data
+    global ssp585_co2_data
+    for_co2_data = {}
+    ssp585_co2_data = {}
+
+    # Load data
+    for model in MODELS:
+        print(f"Loading model {model}")
+        if load_cdo:
+            table = A_OR_AER[model]
+            for_co2_files = sorted(get_filenames(
+                    'LUMIP',
+                    INSTIT[model],
+                    model,
+                    'esm-ssp585-ssp126Lu',
+                    ENSEMBLES[model],
+                    table,
+                    'co2',
+                    ))
+            for_co2_files = '[ ' + ' '.join(for_co2_files) + ' ]'
+            for_co2_data[model] = global_average(var='co2', input=for_co2_files)
+
+            ssp585_co2_files = sorted(get_filenames(
+                    'C4MIP',
+                    INSTIT[model],
+                    model,
+                    'esm-ssp585',
+                    ENSEMBLES[model],
+                    table,
+                    'co2',
+                    ))
+            ssp585_co2_files = '[ ' + ' '.join(ssp585_co2_files) + ' ]'
+            ssp585_co2_data[model] = global_average(var='co2', input=ssp585_co2_files)
+
+            np.save(f'{DATA_DIR}/{model}_co2_for.npy', for_co2_data[model].data)
+            np.save(f'{DATA_DIR}/{model}_co2_ssp585.npy', ssp585_co2_data[model].data)
+        else:
+            for_co2_data[model] = np.load(f'{DATA_DIR}/{model}_co2_for.npy')
+            ssp585_co2_data[model] = np.load(f'{DATA_DIR}/{model}_co2_ssp585.npy')
+
+    # Plot
+    TO_PPM = 1000000
+    plt.figure
+    for model in MODELS:
+        years = np.arange(2015, 2015+len(for_co2_data[model]))
+        plt.plot(
+                years,
+                for_co2_data[model]*TO_PPM,
+                color=COLORS[INSTIT[model]],
+                label=model+' forestation',
+                )
+        years = np.arange(2015, 2015+len(ssp585_co2_data[model]))
+        plt.plot(
+                years,
+                ssp585_co2_data[model]*TO_PPM,
+                color=COLORS[INSTIT[model]],
+                label=model+' esm-ssp585',
+                linestyle='dashed',
+                )
+    plt.xlabel('Years')
+    plt.ylabel('CO$_2$ mixing ratio (ppm)')
+    plt.legend()
+    plt.savefig(f'{PLOTS_DIR}/models/models_co2.png', dpi=DPI)
+
+
+if __name__ != 'analysis.plot_models_co2':
+    make_co2_models_plot()
+
+    plt.show()
+
