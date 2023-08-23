8dc22f393324d164c0ffc910530350913516ec23
tammasloughran <tammas.loughran@csiro.au>
2023-08-23 17:02:44 +1000

    Addressing review coments on plots

--- analysis/plot_model_trees.py
+++ analysis/plot_model_trees.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-# Plot the tree fraction and areas for all models participating in the LUMIP
-# forestation experiment esm-ssp585-ssp126Lu.
+"""Plot the tree fraction and areas for all models participating in the LUMIP
+forestation experiment esm-ssp585-ssp126Lu.
+"""
 import datetime as dt
 import glob
 import pdb
@@ -94,6 +95,21 @@ M2_TOMILKM2 = 1000000000000
 CMAP = 'bwr'
 
 
+import numpy as _np
+def yearly_mean_from_monthly(data:_np.ndarray)->_np.ndarray:
+    """Calculate a yearly mean on a numpy array of monthly data.
+    The 0th dimension must be time and divisible by 12.
+    """
+    if _np.mod(data.shape[0], 12)!=0:
+        raise ValueError("Not enough months in 0th dimension.")
+    toshape = list(data.shape)
+    toshape.pop(0)
+    toshape.insert(0, 12)
+    toshape.insert(0, int(data.shape[0]/12))
+    fraction_of_year = _np.array([31,28,31,30,31,30,31,31,30,31,30,31])/365.0
+    return _np.average(data.reshape(toshape), axis=1, weights=fraction_of_year)
+
+
 def my_num2date(times:np.ndarray, units:str, calendar:str)->list:
     """Convert numbers to a list of datetime objects.
     """
@@ -110,12 +126,16 @@ def global_mean(data:np.ndarray, lats:np.ndarray)->np.ndarray:
 
 def load_frac(file_names:list)->tuple:
     """Load all required data.
+
+    Returns:
+    - (tree_frac,dates,lats,lons)
     """
     if len(file_names)>1:
         nc_tree_frac = nc.MFDataset(file_names, 'r')
     else:
         nc_tree_frac = nc.Dataset(file_names[0], 'r')
     tree_frac = nc_tree_frac.variables['treeFrac'][:].squeeze()
+    tree_frac[tree_frac.mask] = 0 # Remove missing values
     lats = nc_tree_frac.variables['lat'][:]
     lons = nc_tree_frac.variables['lon'][:]
     times = nc_tree_frac.variables['time']
@@ -133,25 +153,53 @@ def calc_tree_area(tree_frac:np.ndarray, area_file:str)->np.ndarray:
 def plot_global_sum_area()->None:
     """Plot the global sum of tree areas.
     """
-    plt.figure()
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
     for model in MODELS:
-        plt.plot(
-                for_dates[model],
-                for_global_sum[model],
+        if model=='UKESM1-0-LL' or model=='GFDL-ESM4':
+            # Convert from monthly to yearly
+            for_dates_plot = for_dates[model][5:-1:12]
+            ssp585_dates_plot = ssp585_dates[model][5:-1:12]
+            for_global_sum_plot = yearly_mean_from_monthly(for_global_sum[model])
+            ssp585_global_sum_plot = yearly_mean_from_monthly(ssp585_global_sum[model])
+            for_global_mean_plot = yearly_mean_from_monthly(for_global_mean[model])
+            ssp585_global_mean_plot = yearly_mean_from_monthly(ssp585_global_mean[model])
+        else:
+            for_dates_plot = for_dates[model]
+            ssp585_dates_plot = ssp585_dates[model]
+            for_global_sum_plot = for_global_sum[model]
+            ssp585_global_sum_plot = ssp585_global_sum[model]
+            for_global_mean_plot = for_global_mean[model]
+            ssp585_global_mean_plot = ssp585_global_mean[model]
+        ax1.plot(
+                for_dates_plot,
+                for_global_sum_plot,
                 color=COLORS[INSTIT[model]],
                 label=model,
                 )
-        plt.plot(
-                ssp585_dates[model],
-                ssp585_global_sum[model],
+        ax1.plot(
+                ssp585_dates_plot,
+                ssp585_global_sum_plot,
+                color=COLORS[INSTIT[model]],
+                linestyle='dashed',
+                )
+        ax2.plot(
+                for_dates_plot,
+                for_global_mean_plot,
+                color=COLORS[INSTIT[model]],
+                )
+        ax2.plot(
+                for_dates_plot,
+                for_global_mean_plot,
                 color=COLORS[INSTIT[model]],
                 linestyle='dashed',
                 )
         print(model, "Delta F:", for_global_sum[model][-1] - ssp585_global_sum[model][-1])
     plt.xlim(left=for_dates[model][0], right=for_dates[model][-1])
-    plt.ylabel('Area (million km$^2$)')
+    ax1.set_ylabel('Area (million km$^2$)')
+    ax2.set_ylabel('Fraction (%)')
     plt.title('Tree area')
-    plt.legend()
+    ax1.legend(frameon=False)
     plt.savefig(f'{PLOTS_DIR}/tree_area_ssp126_all_models_global_sum.png', dpi=DPI)
 
 
@@ -292,6 +340,7 @@ def plot_map_tree_area_change(indata:dict, name:str)->None:
     cbar_ax = fig.add_axes([0.1,0.085,0.85,0.04])
     plt.colorbar(
             data,
+            ticks=np.arange(-90, 100, 10),
             cax=cbar_ax,
             label='Area (million km$^2$)',
             orientation='horizontal',
@@ -312,6 +361,7 @@ def plot_map_tree_fraction_change(indata:dict, name:str)->None:
             )
     fig.set_size_inches(9, 7)
     axes = axes.flatten()
+    discrete_bins = mpl.colors.BoundaryNorm(boundaries=np.arange(-95, 105, 10), ncolors=256)
     for i,model in enumerate(MODELS):
         plt.sca(axes[i])
         # pcolormesh expects +1 lon.
@@ -323,9 +373,10 @@ def plot_map_tree_fraction_change(indata:dict, name:str)->None:
                 llons,
                 llats,
                 indata[model][-1] - indata[model][0],
+                norm=discrete_bins,
                 cmap=CMAP,
-                vmin=-100,
-                vmax=100,
+                #vmin=-100,
+                #vmax=100,
                 transform=ccrs.PlateCarree(),
                 )
         axes[i].coastlines()
@@ -335,6 +386,7 @@ def plot_map_tree_fraction_change(indata:dict, name:str)->None:
     cbar_ax = fig.add_axes([0.1,0.085,0.85,0.04])
     plt.colorbar(
             data,
+            ticks=np.arange(-90, 100, 10),
             cax=cbar_ax,
             label='Tree fraction (%)',
             orientation='horizontal',
@@ -355,6 +407,7 @@ def plot_map_tree_fraction_difference(indata:dict, indata2:dict)->None:
             )
     fig.set_size_inches(9, 7)
     axes = axes.flatten()
+    discrete_bins = mpl.colors.BoundaryNorm(boundaries=np.arange(-95, 105, 10), ncolors=256)
     for i,model in enumerate(MODELS):
         plt.sca(axes[i])
         # pcolormesh expects +1 lon.
@@ -366,9 +419,10 @@ def plot_map_tree_fraction_difference(indata:dict, indata2:dict)->None:
                 llons,
                 llats,
                 indata[model][-1] - indata2[model][-1],
+                norm=discrete_bins,
                 cmap=CMAP,
-                vmin=-100,
-                vmax=100,
+                #vmin=-100,
+                #vmax=100,
                 transform=ccrs.PlateCarree(),
                 )
         axes[i].coastlines()
@@ -411,7 +465,7 @@ def make_tree_frac_plots()->None:
     plot_global_sum_area()
 
     # Plot global mean fraction.
-    plot_global_mean_frac()
+    #plot_global_mean_frac() # This is now plotted together in Figure 1
 
     # Plot area by latitude.
     plot_area_by_lat()
